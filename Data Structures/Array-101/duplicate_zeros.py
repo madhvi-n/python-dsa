1c105d36b9836bac4db32dad36a7ae62db501d63
Madhvi Ń <madhvinandanwar@gmail.com>
2021-09-19 13:34:06 +0530

    Duplicate zeros

---
+++ Data Structures/Array-101/duplicate_zeros.py
@@ -0,0 +1,42 @@
+"""
+Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+"""
+
+def duplicateZeros(arr):
+    zeros = 0
+    arr_length = len(arr) - 1
+
+    for left in range(arr_length + 1):
+        if left > arr_length - zeros:
+            break
+
+        if arr[left] == 0:
+            if left == arr_length - zeros:
+                arr[arr_length] = 0
+                arr_length -= 1
+                break
+            zeros += 1
+
+    last = arr_length - zeros
+
+    for i in range(last, -1, -1):
+        if arr[i] == 0:
+            arr[i + zeros] = 0
+            zeros -= 1
+            arr[i + zeros] = 0
+        else:
+            arr[i + zeros] = arr[i]
+
+    return arr
+
+def main():
+    arr = [1, 0, 2, 3, 0, 4, 5, 0, 0, 4]
+    print(f"Original Array: {arr}")
+    result = duplicateZeros(arr)
+    print(f"Duplicate zeros: {result}")
+
+
+if __name__ == '__main__':
+    main()
