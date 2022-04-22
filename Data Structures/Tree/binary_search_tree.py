0d862e0c209a4d9adf1065badb8b4a9ab5ab1368
MayaScarlet <madhvinandanwar@gmail.com>
2022-04-22 14:56:09 +0530

    Tree

---
+++ Data Structures/Tree/binary_search_tree.py
@@ -0,0 +1,158 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def delete_child(self, val):
+        if val < self.data:
+            if self.left:
+                self.left.delete_child(val)
+        elif val > self.data:
+            if self.right:
+                self.right.delete_child(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+
+            if self.left is None:
+                return self.right
+
+            if self.right is None:
+                return self.left
+
+            min_val = self.right.find_min()
+            self.data = min_val
+            self.right = self.right.delete_child(min_val)
+        return self
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+
+            if self.left is None:
+                return self.right
+
+            if self.right is None:
+                return self.left
+
+            max_val = self.left.find_max()
+            self.data = max_val
+            self.left = self.left.delete(max_val)
+        return self
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def pre_order_traversal(self):
+        elements = [self.data]
+
+        if self.left:
+            elements += self.left.pre_order_traversal()
+
+        if self.right:
+            elements += self.right.pre_order_traversal()
+
+        return elements
+
+    def post_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.post_order_traversal()
+
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+        return elements
+
+    def calculate_sum(self):
+        left_sum = self.left.calculate_sum() if self.left else 0
+        right_sum = self.right.calculate_sum() if self.right else 0
+        return left_sum + self.data + right_sum
+
+
+def build_tree(elements):
+    print("Building tree with these elements:", elements)
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+
+if __name__ == '__main__':
+    num_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+
+    print(f"20 exists in the tree? {num_tree.search(20)}")
+    print(f"35 exists in the tree? {num_tree.search(35)}")
+
+    print("In order traversal (returns the sorted list)", num_tree.in_order_traversal())
+    print("Pre order traversal: ", num_tree.pre_order_traversal())
+    print("Post order traversal: ", num_tree.post_order_traversal())
+
+    num_tree.delete(20)
+    print(num_tree.in_order_traversal())
+
+    print("Find min: ", num_tree.find_min())
+    print("Find max: ", num_tree.find_max())
+    print("Total: ", num_tree.calculate_sum())
