81bf47b6e2deb24ab01b2278234fd9d0979b9a8b
MayaScarlet <madhvinandanwar@gmail.com>
2022-04-22 15:50:27 +0530

    Arrays

--- Problems/Arrays/element_at_index_after_n_rotations.py
+++ Problems/Arrays/element_at_index_after_n_rotations.py
@@ -3,37 +3,12 @@ Find element at given index after right circular rotations on elements at indice
 """
 
 
-def right_rotation(nums, k):
-    if k == 0:
-        return nums
-
-    n = len(nums)
-
-    k = k % n
-
-    # Reverse full array, then reverse arr[:k] and lastly reverse arr[k:]
-    reverse_array(nums, 0, n - 1)
-    reverse_array(nums, 0, k - 1)
-    reverse_array(nums, k, n - 1)
-    return nums
-
-
-def reverse_array(arr, start, end):
-    while start < end:
-        arr[start], arr[end] = arr[end], arr[start]
-        start, end = start + 1, end - 1
-
-
-def helper(nums, start, end):
-    import collections
-    nums = collections.deque(nums)
-    nums.rotate(1)
-
-
+# Solution works for rotation of elements at indices in the range by k=1 element
 def find_element(arr, ranges, index) -> int:
     for i in range(len(ranges)):
         [start, end] = ranges[i]
-        helper(arr, start, end)
+        last = arr.pop(end)
+        arr.insert(0, last)
         print(arr)
     return arr[index]
 
